7ea13975ca7886612e5c547389d7044bce508bfb
Joseph Hellerstein <jlheller@uw.edu>
2022-01-24 17:04:47 -0800

    Tests for notebooks

---
+++ test_notebooks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Ensures that notebooks can run
+
+# Create the script files
+cd notebooks
+for f in *.ipynb
+  do
+    jupyter nbconvert --to script $f
+  done
+cd ..
+# Execute the script files
+for f in `ls notebooks/*.py`
+  do
+    python $f
+  done
