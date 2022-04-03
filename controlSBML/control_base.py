Fix bugs related to implementation of reduced Jacobian for NonlinearIOSystem

controlSBML/control_base.py
@@ -13,6 +13,7 @@ Notes:
 
 from controlSBML.make_roadrunner import makeRoadrunner
 import controlSBML as ctl
+from controlSBML import util
 
 import control
 import numpy as np
@@ -25,6 +26,7 @@ END_TIME = 5  # Default endtime
 POINTS_PER_TIME = 10
 TIME = "time"
 IS_DEBUG = False
+TIMEPOINT_NULL = 1
 
 
 cleanIt = lambda n: n if n[0] != "[" else n[1:-1]
@@ -48,7 +50,7 @@ class ControlBase(object):
         # Iinitial model calculations
         self.model_reference = model_reference
         self.roadrunner = makeRoadrunner(self.model_reference)
-        self._jacobian_timepoint = None
+        self._jacobian_timepoint = TIMEPOINT_NULL
         self._jacobian_df = None
         # Set defaults
         self.species_names = list(
@@ -56,7 +58,6 @@ class ControlBase(object):
         self.depeendent_names = list(
               set(self.species_names).symmetric_difference(self.state_names))
         if not set(self.state_names) <= set(self.species_names):
-            import pdb; pdb.set_trace()
             raise RuntimeError("State name is not a species name.")
         self.full_stoichiometry_df, self.reduced_stoichiometry_df  \
               = self._makeStoichiometryDF()
@@ -99,11 +100,6 @@ class ControlBase(object):
     def C_df(self):
         return self._makeCDF()
 
-    @staticmethod
-    def _makeDF(mat):
-        df = pd.DataFrame(mat, columns=mat.colnames, index=mat.rownames)
-        return df
-
     def _makeStoichiometryDF(self):
         """
         Creates the reduced stoichiometry matrix and the auxiliary matrix
@@ -114,9 +110,9 @@ class ControlBase(object):
         DataFrame - reduced stoichiometry matrix
         """
         #
-        reduced_stoichiometry_df = self._makeDF(
+        reduced_stoichiometry_df = util.mat2DF(
               self.roadrunner.getReducedStoichiometryMatrix())
-        full_stoichiometry_df = self._makeDF(
+        full_stoichiometry_df = util.mat2DF(
               self.roadrunner.getFullStoichiometryMatrix())
         return full_stoichiometry_df, reduced_stoichiometry_df
 
@@ -132,11 +128,15 @@ class ControlBase(object):
         state_names = self.state_names
         C_df = pd.DataFrame(np.eye(self.num_state), columns=state_names,
               index=state_names)
-        L0 = self.roadrunner.getL0Matrix()
-        if len(L0) > 0:
-            L0_df = pd.DataFrame(L0, columns=L0.colnames, index=L0.rownames)
-            C_df = pd.concat([C_df, L0_df], axis=0)
+        if self.is_reduced:
+            L0 = self.roadrunner.getL0Matrix()
+            if len(L0) > 0:
+                L0_df = pd.DataFrame(L0, columns=L0.colnames, index=L0.rownames)
+                C_df = pd.concat([C_df, L0_df], axis=0)
         C_df = C_df.loc[self.output_names, :]
+        values = C_df.values.flatten()
+        if any([np.isnan(v) for v in values]):
+            import pdb; pdb.set_trace()
         return C_df
 
     @property
@@ -146,7 +146,7 @@ class ControlBase(object):
 
         Parameters
         ----------
-
+:
         Returns
         -------
         dict
@@ -171,7 +171,8 @@ class ControlBase(object):
         -------
         np.ndarray: N X 1
         """
-        return self._makeSer(self.state_names)
+        ser = util.makeRoadrunnerSer(self.roadrunner, self.state_names)
+        return ser
 
     @property
     def output_ser(self):
@@ -182,17 +183,7 @@ class ControlBase(object):
         -------
         np.ndarray: N X 1
         """
-        return self._makeSer(self.output_names)
-
-    def _makeSer(self, names):
-        """
-        Contructs a Series for the names.
-
-        Returns
-        -------
-        pd.Series
-        """
-        return pd.Series(list(self.get(names).values()), index=names)
+        return util.makeSer(self.roadrunner, self.output_names)
 
     @property
     def jacobian_df(self):
@@ -204,7 +195,7 @@ class ControlBase(object):
         -------
         pd.DataFrame, species_names
         """
-        if self.getTime() == self._jacobian_timepoint:
+        if np.isclose(self.getTime(), self._jacobian_timepoint):
             if self._jacobian_df is not None:
                 return self._jacobian_df
         if self.is_reduced:
@@ -240,7 +231,7 @@ class ControlBase(object):
 
     def setTime(self, time):
         self.roadrunner.reset()
-        self._jacobian_timepoint = None
+        self._jacobian_timepoint = TIMEPOINT_NULL
         if time > 0.01:
             _ = self.roadrunner.simulate(0.0, time)
 
@@ -330,11 +321,9 @@ class ControlBase(object):
         -------
         object/dict
         """
-        if isinstance(names, str):
-            return self.roadrunner[names]
         if names is None:
             names = self.roadrunner.keys()
-        return {n: self.roadrunner[n] for n in names}
+        return util.getRoadrunnerValue(self.roadrunner, names)
 
     def set(self, name_dct):
         """
@@ -346,10 +335,7 @@ class ControlBase(object):
             key: str
             value: value
         """
-        for name, value in name_dct.items():
-            if isinstance(value, int):
-                value = float(value)
-            self.roadrunner[name] = value
+        util.setRoadrunnerValue(self.roadrunner, name_dct)
 
     @staticmethod
     def _sortList(super_lst, sub_lst):
@@ -440,6 +426,10 @@ class ControlBase(object):
                 ncol = np.shape(B_mat)[1]
             D_mat = np.repeat(0, nrow*ncol)
             D_mat = np.reshape(D_mat, (nrow, ncol))
+        try:
+            ss = control.StateSpace(A_mat, B_mat, C_mat, D_mat)
+        except:
+            import pdb; pdb.set_trace()
         return control.StateSpace(A_mat, B_mat, C_mat, D_mat)
 
     def makeNonlinearIOSystem(self, name, effector_dct=None):

controlSBML/nonlinear_io_system.py
@@ -2,6 +2,7 @@
 
 import controlSBML.constants as cn
 from controlSBML import msgs
+from controlSBML import util
 
 import control
 import numpy as np
@@ -10,6 +11,7 @@ import pandas as pd
 
 ARG_LST = [cn.TIME, cn.STATE, cn.INPUT, cn.PARAMS]
 MIN_ELAPSED_TIME = 1e-2
+
 
 class NonlinearIOSystem(control.NonlinearIOSystem):
 
@@ -39,9 +41,12 @@ class NonlinearIOSystem(control.NonlinearIOSystem):
         self.ctlsb = ctlsb
         self.do_simulate_on_update = do_simulate_on_update
         # Useful properties
-        self.state_names = ctlsb.state_names
+        self.state_names = ctlsb.species_names
         self.input_names = ctlsb.input_names
         self.output_names = ctlsb.output_names
+        self.num_state = len(self.state_names)
+        self.num_input = len(self.input_names)
+        self.num_output = len(self.output_names)
         #
         self.effector_dct = effector_dct
         if self.effector_dct is None:
@@ -52,7 +57,7 @@ class NonlinearIOSystem(control.NonlinearIOSystem):
                 self.effector_dct[input_name] = input_name
         self._ignored_inputs = []  # Note setable inputs
         # Initialize the controlNonlinearIOSystem object
-        super().__init__(self.updfcn, self.outfcn,
+        super().__init__(self._updfcn, self._outfcn,
               inputs=self.input_names, outputs=self.output_names,
               states=self.state_names, name=name)
 
@@ -67,27 +72,27 @@ class NonlinearIOSystem(control.NonlinearIOSystem):
     def setTime(self, time):
         self.ctlsb.setTime(time)
 
-    def getStateSer(self, time=0):
+    def makeStateSer(self, time=0):
         """
         Gets the values of state at the specified time.
 
         Parameters
         ----------
         time: float
-        
+
         Returns
         -------
         pd.Series
         """
         self.setTime(time)
-        return self.ctlsb.state_ser
+        return util.makeRoadrunnerSer(self.ctlsb.roadrunner, self.state_names)
 
-    def updfcn(self, time, x_vec, u_vec, _):
-        """ 
+    def _updfcn(self, time, x_vec, u_vec, _):
+        """
         Computes the change in state. This is done by having roadrunner
         calculate fluxes. No simulation is run, and so this technique
         may not always work.
-        
+
         Parameters
         ----------
         time: float: time
@@ -122,13 +127,12 @@ class NonlinearIOSystem(control.NonlinearIOSystem):
         dstate_ser = pd.Series(self.ctlsb.get(dstate_names))
         return dstate_ser.values
 
-    def outfcn(self, time, x_vec, u_vec, _): 
-        """ 
+    def _outfcn(self, _, x_vec, __, ___):
+        """
         Calculates the values of outputs.
-        
+
         Parameters
         ----------
-        time: float: time
         x_vec: np.array(float): state vector
         u_vec: np.array(float): input vector (in log10 units)
 
@@ -138,8 +142,9 @@ class NonlinearIOSystem(control.NonlinearIOSystem):
         """
         out_vec = np.repeat(np.nan, self.ctlsb.num_output)
         for out_idx, name in enumerate(self.output_names):
-            state_idx = self.state_names.index(name)
-            out_vec[out_idx] = x_vec[state_idx]
+            if name in self.state_names:
+                state_idx = self.state_names.index(name)
+                out_vec[out_idx] = x_vec[state_idx]
         if np.isnan(np.sum(out_vec)):
             raise ValueError("Outputs could not be calculated.")
         return out_vec

controlSBML/simulate_system.py
@@ -2,8 +2,7 @@
 
 import controlSBML.constants as cn
 import controlSBML as ctl
-import controlSBML.util as util
-from controlSBML.option_management.options import Options
+from controlSBML import util
 
 import control
 import numpy as np
@@ -18,7 +17,7 @@ def makeStateVector(sys, start_time=0):
     ----------
     sys: inherits from control.InputOutputSystem
     start_time: float
-    
+
     Returns
     -------
     list
@@ -28,11 +27,12 @@ def makeStateVector(sys, start_time=0):
         for sub_sys in sys.syslist:
             x_lst.extend(makeStateVector(sub_sys, start_time=start_time))
     elif isinstance(sys, ctl.NonlinearIOSystem):
-        x_lst.extend(sys.getStateSer(start_time).values)
+        x_lst.extend(sys.makeStateSer().values)
     else:
         new_state = list(np.repeat(0, sys.nstates))
         x_lst.extend(new_state)
-    return [float(v) for v in x_lst]
+    result = [float(v) for v in x_lst]
+    return result
 
 def simulateSystem(sys, output_names=None, initial_x_vec=None, u_vec=None,
       start_time=cn.START_TIME, end_time=cn.END_TIME,
@@ -51,7 +51,7 @@ def simulateSystem(sys, output_names=None, initial_x_vec=None, u_vec=None,
     start_time: float (start of simulation)
     end_time: float (end of simulation)
     points_per_time: int (number of simulation timepoints per second)
-    
+
     Returns
     -------
     Timeseries

tests/test_nonlinear_io_system.py
@@ -68,14 +68,14 @@ class TestNonlinearIOSystem(unittest.TestCase):
 
     def runSimulation(self, states=None):
         u_vec = np.repeat(0.0, len(self.ctlsb.input_names))
-        self.ctlsb.setTime(TIMES[0])
-        x_vec = self.ctlsb.state_ser.values
-        dct = {n: [] for n in self.ctlsb.state_names}
+        self.sys.setTime(TIMES[0])
+        x_vec = self.sys.makeStateSer().values
+        dct = {n: [] for n in self.sys.state_names}
         for idx, time in enumerate(TIMES):
             dx_vec = self.sys.updfcn(time, x_vec, u_vec, {})
             new_x_vec = x_vec + DT*dx_vec
             [dct[n].append(v) for n, v in
-                  zip(self.ctlsb.state_names, new_x_vec)]
+                  zip(self.sys.state_names, new_x_vec)]
             x_vec = new_x_vec
         if states is None:
            newDct = dct
@@ -100,11 +100,11 @@ class TestNonlinearIOSystem(unittest.TestCase):
           return
         self.init()
         time = 3
-        self.ctlsb.setTime(time)
-        x_vec = self.ctlsb.state_ser.values
-        u_vec = np.repeat(0, len(self.ctlsb.input_names))
+        self.sys.setTime(time)
+        x_vec = self.sys.makeStateSer().values
+        u_vec = np.repeat(0, len(self.sys.input_names))
         out_vec = self.sys.outfcn(time, x_vec, u_vec, None)
-        self.assertEqual(len(out_vec), len(self.ctlsb.output_names))
+        self.assertEqual(len(out_vec), self.sys.num_output)
 
     def testOutlist(self):
         if IGNORE_TEST:
@@ -112,14 +112,14 @@ class TestNonlinearIOSystem(unittest.TestCase):
         self.init()
         names = self.sys.outlist
         self.assertTrue(all([self.sys.name in n for n in names]))
-        self.assertEqual(len(names), self.ctlsb.num_output)
-#################
+        self.assertEqual(len(names), self.sys.num_output)
+
     def testGetStateSer(self):
         if IGNORE_TEST:
           return
         self.init()
-        ser_0 = self.sys.getStateSer()
-        ser_1 = self.sys.getStateSer(time=1)
+        ser_0 = self.sys.makeStateSer()
+        ser_1 = self.sys.makeStateSer(time=1)
         self.assertGreater(ser_0.loc["S0"], ser_1.loc["S1"])
 
     def testInplist(self):
@@ -128,7 +128,7 @@ class TestNonlinearIOSystem(unittest.TestCase):
         self.init()
         names = self.sys.inplist
         self.assertTrue(all([self.sys.name in n for n in names]))
-        self.assertEqual(len(names), self.ctlsb.num_input)
+        self.assertEqual(len(names), self.sys.num_input)
 
     def _checkWithSimulation(self, df):
         df_rr = self.ctlsb.simulateRoadrunner(start_time=0, end_time=END_TIME,
@@ -144,12 +144,12 @@ class TestNonlinearIOSystem(unittest.TestCase):
     def runInputOutputResponse(self, u_val, end_time=END_TIME):
         u_val = np.array(u_val)
         #
-        x_vec = self.ctlsb.state_ser.values
-        u_vec = np.repeat(u_val, len(self.ctlsb.input_names))
+        x_vec = self.sys.makeStateSer().values
+        u_vec = np.repeat(u_val, self.sys.num_input)
         u_vecs = np.array([np.array(u_vec) for _ in range(NUM_TIME)])
         u_vecs = np.reshape(u_vecs, (1, NUM_TIME))
         t, y = control.input_output_response(self.sys, TIMES, u_vecs, x_vec)
-        df = pd.DataFrame(y, self.ctlsb.output_names)
+        df = pd.DataFrame(y, self.sys.output_names)
         return df.transpose()
 
     def testWithInputOutputResponse(self):